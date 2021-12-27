9de86c3be4a8f1b8fa8759731e327bc7c47a4e9b
Mikhail Sharkov <mvsharkov@gmail.com>
2021-12-27 13:50:14 +0100

    first trial

---
+++ whatilearned.R
@@ -0,0 +1,8 @@
+getwd()
+
+# change your working directory
+setwd()
+
+# set path to the location for raw data files in the dslabs package and list files
+path <- system.file("extdata", package="dslabs")
+list.files(path)
